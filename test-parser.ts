aee9a2ffb6e7c8ee1b3c177a84f8517866357009
neverRare <contactneverrare@gmail.com>
2024-01-15 07:53:32 +0800

    improve error handling in test-parser.ts

--- test-parser.ts
+++ test-parser.ts
@@ -1,4 +1,13 @@
+import { OutputError } from "./src/error.ts";
 import { parser } from "./src/parser.ts";
 
 const input = await Deno.readTextFile("./test.txt");
-console.log(JSON.stringify(parser(input), null, 2));
+console.log(
+  JSON.stringify(parser(input), (key, value) => {
+    if (key === "error") {
+      return (value as OutputError).message;
+    } else {
+      return value;
+    }
+  }, 2),
+);
