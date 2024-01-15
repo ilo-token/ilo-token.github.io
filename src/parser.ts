ce8f757124a0a2f4d2486d384bdc295ff3ea47db
neverRare <contactneverrare@gmail.com>
2024-01-15 12:23:06 +0800

    add todo

--- src/parser.ts
+++ src/parser.ts
@@ -56,6 +56,7 @@ function match(regex: RegExp): Parser<RegExpMatchArray> {
     if (match) {
       return new Output([{ value: match, rest: src.slice(match[0].length) }]);
     } else if (src === "") {
+      // TODO: replace this error, this isn't exactly unreachable
       return new Output(new UnreachableError());
     } else {
       const token = src.match(/(.*)(?:\s|$)/)?.[1];
