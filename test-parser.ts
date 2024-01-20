380d38f1979774729169f93225c3092291c4b9fb
neverRare <contactneverrare@gmail.com>
2024-01-20 19:42:41 +0800

    output AST count

--- test-parser.ts
+++ test-parser.ts
@@ -2,7 +2,9 @@ import { OutputError } from "./src/error.ts";
 import { parser } from "./src/parser.ts";
 
 const input = await Deno.readTextFile("./test.txt");
-console.log(JSON.stringify(parser(input), (key, value) => {
+const output = parser(input);
+console.log(JSON.stringify(output, (key, value) => {
   if (key === "error") return (value as null | OutputError)?.message;
   else return value;
 }, 2));
+console.log(`The output has ${output.output.length} AST's`);
