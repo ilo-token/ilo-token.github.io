c389d4364c2f73edd4de849167496c4e9f12f14b
neverRare <contactneverrare@gmail.com>
2024-01-20 19:03:44 +0800

    make formatting more consistent

--- src/ast.ts
+++ src/ast.ts
@@ -10,33 +10,41 @@ export type Modifier =
  * Represents a phrase including preverbial phrases, quotations, and
  * prepositional phrases intended for predicate.
  */
-export type Phrase = {
-  type: "default";
-  headWord: string;
-  alaQuestion: boolean;
-  modifiers: Array<Modifier>;
-} | {
-  type: "cardinal";
-  number: Array<string>;
-  modifiers: Array<Modifier>;
-} | {
-  type: "preverb";
-  preverb: string;
-  alaQuestion: boolean;
-  modifiers: Array<Modifier>;
-  phrase: Phrase;
-} | {
-  type: "preposition";
-  preposition: Preposition;
-} | {
-  type: "quotation";
-  quotation: Quotation;
-};
+export type Phrase =
+  | {
+    type: "default";
+    headWord: string;
+    alaQuestion: boolean;
+    modifiers: Array<Modifier>;
+  }
+  | {
+    type: "cardinal";
+    number: Array<string>;
+    modifiers: Array<Modifier>;
+  }
+  | {
+    type: "preverb";
+    preverb: string;
+    alaQuestion: boolean;
+    modifiers: Array<Modifier>;
+    phrase: Phrase;
+  }
+  | {
+    type: "preposition";
+    preposition: Preposition;
+  }
+  | {
+    type: "quotation";
+    quotation: Quotation;
+  };
 /** Represents multiple phrases separated by repeated particle or _anu_. */
-export type MultiplePhrases = { type: "single"; phrase: Phrase } | {
-  type: "and conjunction";
-  phrases: Array<MultiplePhrases>;
-} | { type: "anu"; phrases: Array<MultiplePhrases> };
+export type MultiplePhrases =
+  | { type: "single"; phrase: Phrase }
+  | {
+    type: "and conjunction";
+    phrases: Array<MultiplePhrases>;
+  }
+  | { type: "anu"; phrases: Array<MultiplePhrases> };
 /** Represents a single prepositional phrase. */
 export type Preposition = {
   preposition: string;
@@ -57,24 +65,30 @@ export type MultiplePredicates =
   | { type: "and conjunction"; predicates: Array<MultiplePredicates> }
   | { type: "anu"; predicates: Array<MultiplePredicates> };
 /** Represents a simple clause. */
-export type Clause = { type: "phrases"; phrases: MultiplePhrases } | {
-  type: "o vocative";
-  phrases: MultiplePhrases;
-} | {
-  type: "li clause";
-  subjects: MultiplePhrases;
-  predicates: MultiplePredicates;
-} | {
-  type: "o clause";
-  subjects: null | MultiplePhrases;
-  predicates: MultiplePredicates;
-} | {
-  type: "prepositions";
-  prepositions: Array<Preposition>;
-} | {
-  type: "quotation";
-  quotation: Quotation;
-};
+export type Clause =
+  | { type: "phrases"; phrases: MultiplePhrases }
+  | {
+    type: "o vocative";
+    phrases: MultiplePhrases;
+  }
+  | {
+    type: "li clause";
+    subjects: MultiplePhrases;
+    predicates: MultiplePredicates;
+  }
+  | {
+    type: "o clause";
+    subjects: null | MultiplePhrases;
+    predicates: MultiplePredicates;
+  }
+  | {
+    type: "prepositions";
+    prepositions: Array<Preposition>;
+  }
+  | {
+    type: "quotation";
+    quotation: Quotation;
+  };
 /** Represents a clause including preclause and postclause. */
 export type FullClause = {
   taso: boolean;
