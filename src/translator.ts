551bdaa5d6902b49001b24f1ef809ab8b33c706c
neverRare <contactneverrare@gmail.com>
2024-01-21 17:48:11 +0800

    comments and exports

--- src/translator.ts
+++ src/translator.ts
@@ -2,10 +2,14 @@ import { Sentence } from "./ast.ts";
 import { Output } from "./output.ts";
 import { parser } from "./parser.ts";
 
-type TranslationOutput = Output<string>;
+/** A special kind of Output that translators returns. */
+export type TranslationOutput = Output<string>;
+
+/** Translates a single sentence. */
 function translateSentence(sentence: Sentence): TranslationOutput {
   throw new Error("todo");
 }
+/** Translates multiple sentences. */
 function translateSentences(sentences: Array<Sentence>): TranslationOutput {
   return sentences.reduce(
     (output, sentence) =>
@@ -21,6 +25,7 @@ function translateSentences(sentences: Array<Sentence>): TranslationOutput {
     new Output([""]),
   );
 }
-function translate(src: string): TranslationOutput {
+/** Full Toki Pona translator. */
+export function translate(src: string): TranslationOutput {
   return parser(src).flatMap(translateSentences);
 }
