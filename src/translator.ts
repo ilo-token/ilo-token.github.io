0a84cf4ac0e070d7acea069ab78303ad7dbe99a3
neverRare <contactneverrare@gmail.com>
2024-01-21 17:39:23 +0800

    implement translator for multiple sentences (sentence translator itself is TODO)

--- src/translator.ts
+++ src/translator.ts
@@ -1,12 +1,26 @@
 import { Sentence } from "./ast.ts";
 import { Output } from "./output.ts";
 import { parser } from "./parser.ts";
+
 type TranslationOutput = Output<string>;
-function translateSentence(output: Sentence): TranslationOutput {
+function translateSentence(sentence: Sentence): TranslationOutput {
   throw new Error("todo");
 }
-function translate(src: string): TranslationOutput {
-  return parser(src).flatMap((sentences) =>
-    new Output(sentences).flatMap(translateSentence)
+function translateSentences(sentences: Array<Sentence>): TranslationOutput {
+  return sentences.reduce(
+    (output, sentence) =>
+      output.flatMap((left) =>
+        translateSentence(sentence).map((right) => {
+          if (left === "") {
+            return right;
+          } else {
+            return [left, right].join(" ");
+          }
+        })
+      ),
+    new Output([""]),
   );
+}
+function translate(src: string): TranslationOutput {
+  return parser(src).flatMap(translateSentences);
 }
