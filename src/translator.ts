28dde3e248ce8e033c0fdc4ff6ceb2a3da315e8e
neverRare <contactneverrare@gmail.com>
2024-01-23 12:44:29 +0800

    simplify translator functions

--- src/translator.ts
+++ src/translator.ts
@@ -5,51 +5,42 @@ import { parser } from "./parser.ts";
 /** A special kind of Output that translators returns. */
 export type TranslationOutput = Output<string>;
 
+/**
+ * Helper function for turning array or tuple of Output into Output of array or
+ * tuple.
+ */
+// TODO: maybe there's a better name
+function rotate<T extends Array<unknown>>(
+  array: { [I in keyof T]: Output<T[I]> } & { length: T["length"] },
+): Output<T> {
+  // We resorted to using `any` types here, make sure it works properly
+  return array.reduce(
+    // deno-lint-ignore no-explicit-any
+    (result: Output<any>, output) =>
+      result.flatMap((left) => output.map((right) => [...left, right])),
+    // deno-lint-ignore no-explicit-any
+    new Output<any>([[]]),
+  ) as Output<T>;
+}
 function translateFullClause(fullClause: FullClause): TranslationOutput {
   throw new Error("todo");
 }
 /** Translates a single sentence. */
 function translateSentence(sentence: Sentence): TranslationOutput {
-  const laClauses = sentence.laClauses;
-  const contexts = laClauses.slice(0, laClauses.length - 1);
-  const final = laClauses[laClauses.length - 1];
-  const contextTranslation = contexts.reduce(
-    (output, context) =>
-      output.flatMap((left) =>
-        translateFullClause(context).map((right) => {
-          if (left === "") {
-            return `given ${right}, `;
-          } else {
-            return `${left}given ${right}`;
-          }
-        })
-      ),
-    new Output([""]),
-  );
-  return contextTranslation.flatMap((contexts) =>
-    translateFullClause(final).map((final) => {
-      if (contexts === "") {
-        return [final, sentence.punctuation].join("");
-      } else {
-        return [contexts, final, sentence.punctuation].join("");
-      }
-    })
-  );
+  return rotate(sentence.laClauses.map(translateFullClause)).map((clauses) => {
+    const contexts = clauses.slice(0, clauses.length - 1);
+    const final = clauses[clauses.length - 1];
+    return [
+      ...contexts.map((context) => `given ${context}, `),
+      final,
+      sentence.punctuation,
+    ].join("");
+  });
 }
 /** Translates multiple sentences. */
 function translateSentences(sentences: Array<Sentence>): TranslationOutput {
-  return sentences.reduce(
-    (output, sentence) =>
-      output.flatMap((left) =>
-        translateSentence(sentence).map((right) => {
-          if (left === "") {
-            return right;
-          } else {
-            return [left, right].join(" ");
-          }
-        })
-      ),
-    new Output([""]),
+  return rotate(sentences.map(translateSentence)).map((sentences) =>
+    sentences.join(" ")
   );
 }
 /** Full Toki Pona translator. */
