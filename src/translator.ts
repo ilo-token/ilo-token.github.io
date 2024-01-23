9b5fd29b6b3dc1da7943c398598f7b543e775380
neverRare <contactneverrare@gmail.com>
2024-01-23 11:53:19 +0800

    implement translator for a single sentence

--- src/translator.ts
+++ src/translator.ts
@@ -1,13 +1,40 @@
-import { Sentence } from "./ast.ts";
+import { FullClause, Sentence } from "./ast.ts";
 import { Output } from "./output.ts";
 import { parser } from "./parser.ts";
 
 /** A special kind of Output that translators returns. */
 export type TranslationOutput = Output<string>;
 
+function translateFullClause(fullClause: FullClause): TranslationOutput {
+  throw new Error("todo");
+}
 /** Translates a single sentence. */
 function translateSentence(sentence: Sentence): TranslationOutput {
-  throw new Error("todo");
+  const laClauses = sentence.laClauses;
+  const contexts = laClauses.slice(0, laClauses.length - 1);
+  const final = laClauses[laClauses.length - 1];
+  const contextTranslation = contexts.reduce(
+    (output, context) =>
+      output.flatMap((left) =>
+        translateFullClause(context).map((right) => {
+          if (left === "") {
+            return `given ${right}, `;
+          } else {
+            return `${left}given ${right}`;
+          }
+        })
+      ),
+    new Output([""]),
+  );
+  return contextTranslation.flatMap((contexts) =>
+    translateFullClause(final).map((final) => {
+      if (contexts === "") {
+        return [final, sentence.punctuation].join("");
+      } else {
+        return [contexts, final, sentence.punctuation].join("");
+      }
+    })
+  );
 }
 /** Translates multiple sentences. */
 function translateSentences(sentences: Array<Sentence>): TranslationOutput {
