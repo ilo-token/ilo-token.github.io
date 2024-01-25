73567704faebae1e39081e9e096ef1b0a11657d1
neverRare <contactneverrare@gmail.com>
2024-01-25 18:35:58 +0800

    update definition for taso

--- src/definition.ts
+++ src/definition.ts
@@ -1515,7 +1515,7 @@ export const DEFINITION: { [key: string]: Translation } = {
   taso: {
     noun: [],
     adjective: ["only"],
-    adverb: [],
+    adverb: ["only"],
     gerundVerb: [],
     pastTransitive: [],
     presentTransitive: [],
