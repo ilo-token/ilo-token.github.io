9dac9e758eed2fb888209db52ba5fd714f0c183f
neverRare <contactneverrare@gmail.com>
2024-02-02 10:39:11 +0800

    prepare for watching files

--- bundle.ts
+++ bundle.ts
@@ -3,8 +3,14 @@ import { emit } from "./dev-deps.ts";
 const SOURCE = "./src/main.ts";
 const DESTINATION = "./main.js";
 
-const url = new URL(SOURCE, import.meta.url);
-const result = await emit.bundle(url, { minify: true });
+if (Deno.args[0] === "build") {
+  const url = new URL(SOURCE, import.meta.url);
+  const result = await emit.bundle(url, { minify: true });
 
-const { code } = result;
-await Deno.writeTextFile(DESTINATION, code);
+  const { code } = result;
+  await Deno.writeTextFile(DESTINATION, code);
+} else if (Deno.args[0] === "watch") {
+  throw new Error("todo");
+} else {
+  throw new Error(`Unrecognized build option, ${Deno.args[0]}`);
+}
