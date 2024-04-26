d0d566666756775b78dd6bf82dc3bd5ca9a3d4c4
neverRare <contactneverrare@gmail.com>
2024-04-26 14:51:49 +0800

    partial implementation of single word translator

--- src/translator.ts
+++ src/translator.ts
@@ -1,12 +1,34 @@
 import { parse } from "./ast-parser.ts";
 import * as TokiPona from "./ast.ts";
+import { SPECIAL_CONTENT_WORD_DEFINITION } from "./dictionary.ts";
+import { CONTENT_WORD_DEFINITION } from "./dictionary.ts";
+import { PREPOSITION_DEFINITION } from "./dictionary.ts";
+import { PARTICLE_DEFINITION } from "./dictionary.ts";
 import * as English from "./english-ast.ts";
+import { TodoError, UnreachableError } from "./error.ts";
 import { Output } from "./output.ts";
 
 function multipleSentences(
   sentences: TokiPona.MultipleSentences,
 ): Output<English.Sentence> {
-  throw new Error("todo");
+  if (sentences.type === "single word") {
+    const word = sentences.word;
+    return new Output([
+      ...PARTICLE_DEFINITION[word] ?? [],
+      ...SPECIAL_CONTENT_WORD_DEFINITION[word] ?? [],
+      ...PREPOSITION_DEFINITION[word] ?? [],
+      // TODO: Preverb
+      // TODO: Content word definition
+    ]).map((definition) => ({
+      dependentClauses: [],
+      independentClause: { type: "free form", text: definition },
+      punctuation: "",
+    }));
+  } else if (sentences.type === "sentences") {
+    return new Output(new TodoError("translation of sentences"));
+  } else {
+    throw new UnreachableError();
+  }
 }
 export function translate(src: string): Output<English.Sentence> {
   return parse(src).flatMap(multipleSentences);
