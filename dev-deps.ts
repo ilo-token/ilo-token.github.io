5271cc2cf5cb1a0f0ca587023ba3224ec695322d
neverRare <contactneverrare@gmail.com>
2024-05-01 16:35:56 +0800

    update dependencies

--- dev-deps.ts
+++ dev-deps.ts
@@ -1,3 +1,3 @@
-export * as Emit from "https://deno.land/x/emit@0.38.2/mod.ts";
-export * as Debounce from "https://deno.land/std@0.218.2/async/debounce.ts";
+export * as Emit from "https://deno.land/x/emit@0.40.0/mod.ts";
+export * as Debounce from "https://deno.land/std@0.224.0/async/debounce.ts";
 export * as TeloMisikeke from "./telo-misikeke/build.ts";
