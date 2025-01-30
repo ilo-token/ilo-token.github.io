4119f685ba8a3928e72977b80b8b8c2e24c419a5
Koko <neverrare@proton.me>
2025-01-30 12:06:54 +0800

    this is the idiom i guess

--- src/main-deno.ts
+++ src/main-deno.ts
@@ -1,12 +1,14 @@
 import { translate } from "./mod.ts";
 
-while (true) {
-  const input = prompt("> ");
-  if (input == null) {
-    break;
-  }
-  const output = translate(input);
-  for (const translation of output) {
-    console.log(`  - ${translation}`);
+if (import.meta.main) {
+  while (true) {
+    const input = prompt("> ");
+    if (input == null) {
+      break;
+    }
+    const output = translate(input);
+    for (const translation of output) {
+      console.log(`  - ${translation}`);
+    }
   }
 }
