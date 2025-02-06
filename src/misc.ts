9030749ff8108e9c5769fcbb167b3e5d97449680
Koko <neverrare@proton.me>
2025-02-06 11:30:47 +0800

    update storage detection

--- src/misc.ts
+++ src/misc.ts
@@ -47,19 +47,10 @@ export function checkLocalStorage(): boolean {
         localStorageAvailable = true;
       } catch (e) {
         localStorageAvailable = e instanceof DOMException &&
-          // everything except Firefox
-          (e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === "QuotaExceededError" ||
-            // Firefox
-            e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
-          // acknowledge QuotaExceededError only if there's something already
-          // stored
+          e.name === "QuotaExceededError" &&
+          // acknowledge QuotaExceededError only if there's something already stored
           localStorage &&
-          localStorage.length > 0;
+          localStorage.length !== 0;
       }
     }
   }
