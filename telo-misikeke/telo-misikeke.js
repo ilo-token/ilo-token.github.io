85f6ddfc617a5a58ee2b891a3325b99352516e7f
Koko <neverrare@proton.me>
2025-02-06 09:07:37 +0800

    small improvement

--- telo-misikeke/telo-misikeke.js
+++ telo-misikeke/telo-misikeke.js
@@ -3,7 +3,7 @@
 import { ParserWithCallbacks } from "./Parser.js";
 import { build_rules, getMessage } from "./rules.js";
 import LINKU from "./linku-data.json" with { type: "json" };
-import { escapeHtml, newlineAsHtml } from "../src/misc.ts";
+import { escapeHtmlWithNewline, newlineAsHtml } from "../src/misc.ts";
 
 const RULES = build_rules(LINKU);
 
@@ -13,7 +13,7 @@ export function errors(text) {
     .tokenize(text)
     .filter((token) => RULES[token.ruleName].category === "error")
     .map((token) => {
-      const src = escapeHtml(token.text);
+      const src = escapeHtmlWithNewline(token.text);
       const message = newlineAsHtml(getMessage(token.ruleName, token.match));
       return `"${src}" ${message}`;
     });
