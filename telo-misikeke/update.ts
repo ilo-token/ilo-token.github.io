8c677363a3797cec8bf3c2373b2a70156b74b6e1
Koko <neverrare@proton.me>
2025-02-08 18:29:53 +0800

    small fix

--- telo-misikeke/update.ts
+++ telo-misikeke/update.ts
@@ -73,5 +73,5 @@ if (import.meta.main) {
         buildCode(file.source, file.destination, file.exportItems)
       ),
   ]);
-  console.log("updated telo misikeke");
+  console.log("Updated telo misikeke");
 }
