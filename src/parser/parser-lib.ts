95c415fc4d8eb1c03c98bba4f958207c23d33674
Koko <neverrare@proton.me>
2025-02-26 19:29:20 +0800

    improve code

--- src/parser/parser-lib.ts
+++ src/parser/parser-lib.ts
@@ -18,9 +18,7 @@ export class Parser<T> {
   static cache: null | Cache = null;
   constructor(parser: (src: string) => ParserResult<T>) {
     const cache = new Map<string, ParserResult<T>>();
-    if (Parser.cache != null) {
-      Parser.cache.add(cache);
-    }
+    Parser.cache?.add(cache);
     this.#parser = memoize(parser, { cache });
   }
   parser(src: string): ParserResult<T> {
