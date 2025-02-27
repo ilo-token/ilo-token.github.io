6131e9d4095a482bd1c4a465d1c173dca0b1ed4f
Koko <neverrare@proton.me>
2025-02-27 11:57:45 +0800

    improve repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,5 @@
+import { unescape } from "@std/html/entities";
+import entityList from "@std/html/named-entity-list.json" with { type: "json" };
 import { repeatArray } from "./misc.ts";
 import { translate } from "./mod.ts";
 
@@ -14,8 +16,11 @@ if (import.meta.main) {
       const arrayResult = translate(input);
       for (const translation of arrayResult) {
         const count = translation.match(/<strong>/g)?.length ?? 0;
+        const text = unescape(translation.replaceAll(/<\/?strong>/g, "%c"), {
+          entityList,
+        });
         console.log(
-          `  - ${translation.replaceAll(/<\/?strong>/g, "%c")}`,
+          `  - ${text}`,
           ...repeatArray(["font-weight: bold", ""], count).flat(),
         );
       }
