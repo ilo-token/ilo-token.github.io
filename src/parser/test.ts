e4f80292385844a50597c2b525a8becbd78fafd6
Koko <neverrare@proton.me>
2025-03-27 19:15:34 +0800

    fix ucsur mapping

--- src/parser/test.ts
+++ src/parser/test.ts
@@ -1,9 +1,12 @@
 // This code is Deno only
 
+import { assertEquals } from "@std/assert/equals";
 import { assertNotEquals } from "@std/assert/not-equals";
 import { assertThrows } from "@std/assert/throws";
-import { parse } from "./parser.ts";
 import { EXAMPLE_SENTENCES, MALFORMED_SENTENCES } from "../examples.ts";
+import { parse } from "./parser.ts";
+import { KU_LILI, KU_SULI, PU } from "./ucsur.ts";
+import { assert } from "@std/assert/assert";
 
 Deno.test("AST all distinct", () => {
   for (const sentence of EXAMPLE_SENTENCES) {
@@ -17,6 +20,27 @@ Deno.test("AST all distinct", () => {
 Deno.test("parser all error", () => {
   for (const sentence of MALFORMED_SENTENCES) {
     assertThrows(() => parse(sentence).unwrap());
+  }
+});
+
+Deno.test("ucsur have proper length", () => {
+  assertEquals(PU.length, 120);
+  assertEquals(KU_SULI.length, 17);
+  assertEquals(KU_LILI.length, 4);
+});
+
+Deno.test("ucsur ordered", () => {
+  for (const [i, word] of PU.entries()) {
+    if (i < PU.length - 1) {
+      const other = PU[i + 1];
+      assert(word < PU[i + 1], `error between ${word} and ${other}`);
+    }
+  }
+});
+
+Deno.test("no ali", () => {
+  for (const word of PU) {
+    assertNotEquals(word, "ali");
   }
 });
 
