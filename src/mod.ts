9d32e40b911b825b30456de96dc52a24325be24e
Koko <neverrare@proton.me>
2025-03-27 17:27:21 +0800

    update exports and imports

--- src/mod.ts
+++ src/mod.ts
@@ -1,17 +1,20 @@
 import { distinct } from "@std/collections/distinct";
 import { shuffle } from "@std/random/shuffle";
+import { deduplicateErrors } from "../misc/deduplicate_errors.ts";
 import { errors } from "../telo_misikeke/telo_misikeke.js";
 import { ArrayResultError } from "./array_result.ts";
 import { settings } from "./settings.ts";
 import { translate as rawTranslate } from "./translator/translator.ts";
-import { deduplicateErrors } from "../misc/deduplicate_errors.ts";
 
-export { ArrayResultError } from "./array_result.ts";
-export type { ArrayResultOptions } from "./array_result.ts";
+export { ArrayResultError, type ArrayResultOptions } from "./array_result.ts";
 export { loadCustomDictionary } from "./dictionary.ts";
 export { clearCache } from "./parser/cache.ts";
-export { defaultSettings, settings } from "./settings.ts";
-export type { RedundancySettings, Settings } from "./settings.ts";
+export {
+  defaultSettings,
+  type RedundancySettings,
+  type Settings,
+  settings,
+} from "./settings.ts";
 
 export function translate(tokiPona: string): ReadonlyArray<string> {
   const arrayResult = rawTranslate(tokiPona);
