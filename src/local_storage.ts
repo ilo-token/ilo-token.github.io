69e83987c34420e86407ba95bced5b6ade6f470e
Koko <neverrare@proton.me>
2025-04-27 08:00:57 +0800

    add type annotation

--- src/local_storage.ts
+++ src/local_storage.ts
@@ -1,6 +1,6 @@
 import { lazy } from "../misc/misc.ts";
 
-export const checkLocalStorage = lazy(() => {
+export const checkLocalStorage = lazy((): boolean => {
   // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
   try {
     const x = "__storage_test__";
