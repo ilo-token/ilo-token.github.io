861e2e4f7a1ad10fd5b30bb97bfd576b3df72c43
Koko <neverrare@proton.me>
2025-04-27 16:23:58 +0800

    reimplement back shuffling

--- src/translator/translator.ts
+++ src/translator/translator.ts
@@ -1,9 +1,12 @@
+import { shuffle } from "@std/random/shuffle";
 import { errors } from "../../telo_misikeke/telo_misikeke.js";
 import { IterableResult, ResultError } from "../compound.ts";
 import { parser } from "../parser/parser.ts";
 import { settings } from "../settings.ts";
 import * as EnglishComposer from "./composer.ts";
 import { multipleSentences } from "./sentence.ts";
+
+const RANDOMIZATION_LIMIT = 10000;
 
 export function translate(tokiPona: string): IterableResult<string> {
   return new IterableResult(function* () {
@@ -15,18 +18,44 @@ export function translate(tokiPona: string): IterableResult<string> {
     let yielded = false;
     const aggregateErrors: Array<ResultError> = [];
     const unique: Set<string> = new Set();
-    for (const result of iterableResult.iterable()) {
-      switch (result.type) {
-        case "value":
-          if (!unique.has(result.value)) {
-            yielded = true;
-            yield result;
+    if (settings.randomize) {
+      let i = 0;
+      for (const result of iterableResult.iterable()) {
+        if (i >= RANDOMIZATION_LIMIT) {
+          yield {
+            type: "error",
+            error: new ResultError("too much output to shuffle"),
+          };
+          return;
+        }
+        switch (result.type) {
+          case "value":
             unique.add(result.value);
-          }
-          break;
-        case "error":
-          aggregateErrors.push(result.error);
-          break;
+            i++;
+            break;
+          case "error":
+            aggregateErrors.push(result.error);
+            break;
+        }
+      }
+      for (const value of shuffle([...unique])) {
+        yielded = true;
+        yield { type: "value", value };
+      }
+    } else {
+      for (const result of iterableResult.iterable()) {
+        switch (result.type) {
+          case "value":
+            if (!unique.has(result.value)) {
+              yielded = true;
+              yield result;
+              unique.add(result.value);
+            }
+            break;
+          case "error":
+            aggregateErrors.push(result.error);
+            break;
+        }
       }
     }
     if (!yielded) {
