dba39c586a23fae4f5b09d7eebf40243fda84322
neverRare <neverrare@proton.me>
2026-01-11 12:09:33 +0800

    Format

--- src/translator2/modifier.ts
+++ src/translator2/modifier.ts
@@ -2,29 +2,29 @@ import * as Dictionary from "../../dictionary/type.ts";
 import * as English from "./ast.ts";
 
 export type ModifierTranslation =
-    | Readonly<{ type: "noun"; noun: English.NounPhrase }>
-    | Readonly<{
+  | Readonly<{ type: "noun"; noun: English.NounPhrase }>
+  | Readonly<{
     type: "noun preposition";
     noun: English.NounPhrase;
     preposition: string;
-}>
-    | Readonly<{ type: "adjective"; adjective: English.AdjectivePhrase }>
-    | Readonly<{ type: "determiner"; determiner: Dictionary.Determiner }>
-    | Readonly<{ type: "adverb"; adverb: English.Adverb }>
-    | Readonly<{ type: "name"; name: string }>;
+  }>
+  | Readonly<{ type: "adjective"; adjective: English.AdjectivePhrase }>
+  | Readonly<{ type: "determiner"; determiner: Dictionary.Determiner }>
+  | Readonly<{ type: "adverb"; adverb: English.Adverb }>
+  | Readonly<{ type: "name"; name: string }>;
 export type AdjectivalModifier = Readonly<{
-    nounPreposition:
-        | null
-        | Readonly<{ noun: English.NounPhrase; preposition: string }>;
-    determiners: ReadonlyArray<Dictionary.Determiner>;
-    adjectives: ReadonlyArray<English.AdjectivePhrase>;
-    name: null | string;
-    ofPhrase: ReadonlyArray<English.NounPhrase>;
+  nounPreposition:
+    | null
+    | Readonly<{ noun: English.NounPhrase; preposition: string }>;
+  determiners: ReadonlyArray<Dictionary.Determiner>;
+  adjectives: ReadonlyArray<English.AdjectivePhrase>;
+  name: null | string;
+  ofPhrase: ReadonlyArray<English.NounPhrase>;
 }>;
 export type AdverbialModifier = Readonly<{
-    adverbs: ReadonlyArray<English.Adverb>;
-    inWayPhrase: null | English.NounPhrase;
+  adverbs: ReadonlyArray<English.Adverb>;
+  inWayPhrase: null | English.NounPhrase;
 }>;
 export type MultipleModifierTranslation =
-    | (Readonly<{ type: "adjectival" }> & AdjectivalModifier)
-    | (Readonly<{ type: "adverbial" }> & AdverbialModifier);
+  | (Readonly<{ type: "adjectival" }> & AdjectivalModifier)
+  | (Readonly<{ type: "adverbial" }> & AdverbialModifier);
