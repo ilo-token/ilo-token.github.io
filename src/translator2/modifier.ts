41aea5a4b7dcc640851032250a1ab302953be4ee
neverRare <neverrare@proton.me>
2026-01-11 12:04:50 +0800

    More types

--- src/translator2/modifier.ts
+++ src/translator2/modifier.ts
@@ -12,3 +12,19 @@ export type ModifierTranslation =
     | Readonly<{ type: "determiner"; determiner: Dictionary.Determiner }>
     | Readonly<{ type: "adverb"; adverb: English.Adverb }>
     | Readonly<{ type: "name"; name: string }>;
+export type AdjectivalModifier = Readonly<{
+    nounPreposition:
+        | null
+        | Readonly<{ noun: English.NounPhrase; preposition: string }>;
+    determiners: ReadonlyArray<Dictionary.Determiner>;
+    adjectives: ReadonlyArray<English.AdjectivePhrase>;
+    name: null | string;
+    ofPhrase: ReadonlyArray<English.NounPhrase>;
+}>;
+export type AdverbialModifier = Readonly<{
+    adverbs: ReadonlyArray<English.Adverb>;
+    inWayPhrase: null | English.NounPhrase;
+}>;
+export type MultipleModifierTranslation =
+    | (Readonly<{ type: "adjectival" }> & AdjectivalModifier)
+    | (Readonly<{ type: "adverbial" }> & AdverbialModifier);
