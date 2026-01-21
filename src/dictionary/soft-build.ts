59001b332a321135039f45b3bfc8629e66e2c6cb
neverRare <neverrare@proton.me>
2026-01-21 15:28:45 +0800

    fix

--- src/dictionary/soft-build.ts
+++ src/dictionary/soft-build.ts
@@ -1,11 +1,13 @@
 // this code is Deno only
 
 import { exists } from "@std/fs/exists";
+import { Parser } from './parallel_parser.ts'
 
 if (import.meta.main) {
+  using parser = new Parser();
   if (!await exists(new URL("./global_dictionary.ts", import.meta.url))) {
     const Dictionary = await import("./build.ts");
-    if (!await Dictionary.build()) {
+    if (!await Dictionary.build(parser)) {
       await Dictionary.buildWithDictionary(new Map());
       // deno-lint-ignore no-console
       console.error(
